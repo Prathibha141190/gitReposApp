322741ff8eccbef097f575a62e2c93d22cc06ed2
prathibha141190 <prathibhasony5@gmail.com>
2023-11-09 10:26:36 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import ReposDisplay from './components/ReposDisplay'
+import './App.css'
+
+const App = () => <ReposDisplay />
+
+export default App
---
+++ src/components/ReposDisplay/index.js
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+import './index.css'
+
+class ReposDisplay extends Component {
+  render() {
+    return (
+      <>
+        <div className="main_container">
+          <h1 className="main_heading">Most Starred Repos</h1>
+        </div>
+      </>
+    )
+  }
+}
+
+export default ReposDisplay
